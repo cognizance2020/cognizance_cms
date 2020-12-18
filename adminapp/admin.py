40a65567a85540460f61d84a36394d838763ba92
TechieNK <nk2indian@gmail.com>
2020-12-18 10:42:50 +0530

    Demo

--- adminapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Member)
-admin.site.register(Achievement)
-admin.site.register(Task)
--- membersapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MembersappConfig(AppConfig):
-    name = 'membersapp'
